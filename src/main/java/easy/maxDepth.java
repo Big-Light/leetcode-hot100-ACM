4a27f1f72355814e7d9c6129d0a4ec2f301456cd
big-light <xcf941743128@163.com>
2025-02-17 20:49:53 +0800

    104. 二叉树的最大深度

---
+++ src/main/java/easy/maxDepth.java
@@ -0,0 +1,56 @@
+package easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class maxDepth {
+    public static class TreeNode{
+        int val;
+        TreeNode left, right;
+        TreeNode(){};
+        TreeNode(int val){
+            this.val = val;
+        }
+    }
+    public static int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] split = s.split(",");
+        TreeNode root = buildTree(split);
+        System.out.println(maxDepth(root));
+    }
+
+    public static TreeNode buildTree(String[] nodes){
+        if(nodes == null || nodes.length == 0 || nodes[0].equals("null"))
+            return null;
+        TreeNode root = new TreeNode(Integer.parseInt(nodes[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int index = 1;
+        while (index < nodes.length && !queue.isEmpty()){
+            TreeNode cur = queue.poll();
+            if(index < nodes.length && !nodes[index].equals("null")){
+                cur.left = new TreeNode(Integer.parseInt(nodes[index]));
+                queue.offer(cur.left);
+            }
+            index++;
+
+            if(index < nodes.length && !nodes[index].equals("null")){
+                cur.right = new TreeNode(Integer.parseInt(nodes[index]));
+                queue.offer(cur.right);
+            }
+            index++;
+        }
+        return root;
+    }
+}
